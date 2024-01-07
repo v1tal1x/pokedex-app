3be630a939f091384cf29dcd90b0e5b93c56448b
v1tal1x <bvitaliy.work@gmail.com>
2024-01-07 20:32:24 +0200

    feat: Add icons, images and config for them

---
+++ lib/src/config/app_assets.dart
@@ -0,0 +1,22 @@
+abstract class AppAssets {
+  AppAssets._();
+
+  static const String _baseAssetsPath = 'assets';
+  static const String _baseIconsPath = '$_baseAssetsPath/icons';
+  static const String _baseImagesPath = '$_baseAssetsPath/images';
+
+  static const String icArrowBack = '$_baseIconsPath/arrow_back.svg';
+  static const String icChevronLeft = '$_baseIconsPath/chevron_left.svg';
+  static const String icChevronRight = '$_baseIconsPath/chevron_right.svg';
+  static const String icClose = '$_baseIconsPath/close.svg';
+  static const String icPokeball = '$_baseIconsPath/pokeball.svg';
+  static const String icSearch = '$_baseIconsPath/sort.svg';
+  static const String icSort = '$_baseIconsPath/arrow_back.svg';
+  static const String icStraighten = '$_baseIconsPath/straighten.svg';
+  static const String icTag = '$_baseIconsPath/tag.svg';
+  static const String icTextFormat = '$_baseIconsPath/text_format.svg';
+  static const String icWeight = '$_baseIconsPath/weight.svg';
+
+  static const String imgDefaultPokemon =
+      '$_baseImagesPath/dafault_pokemon.png';
+}
