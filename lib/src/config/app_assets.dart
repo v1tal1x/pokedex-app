7b81a1c71cd42906d2d2616ad6c6736aa984a814
v1tal1x <bvitaliy.work@gmail.com>
2024-01-19 21:35:43 +0200

    fix: Icon paths

--- lib/src/config/app_assets.dart
+++ lib/src/config/app_assets.dart
@@ -10,8 +10,8 @@ abstract class AppAssets {
   static const String icChevronRight = '$_baseIconsPath/chevron_right.svg';
   static const String icClose = '$_baseIconsPath/close.svg';
   static const String icPokeball = '$_baseIconsPath/pokeball.svg';
-  static const String icSearch = '$_baseIconsPath/sort.svg';
-  static const String icSort = '$_baseIconsPath/arrow_back.svg';
+  static const String icSearch = '$_baseIconsPath/search.svg';
+  static const String icSort = '$_baseIconsPath/sort.svg';
   static const String icStraighten = '$_baseIconsPath/straighten.svg';
   static const String icTag = '$_baseIconsPath/tag.svg';
   static const String icTextFormat = '$_baseIconsPath/text_format.svg';
