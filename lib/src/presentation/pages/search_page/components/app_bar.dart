e1423aec05d62de096653a2f12a3355eef57297f
v1tal1x <bvitaliy.work@gmail.com>
2024-01-22 22:59:53 +0200

    chore: Add bottom app bar and search widget

--- lib/src/presentation/pages/search_page/components/app_bar.dart
+++ lib/src/presentation/pages/search_page/components/app_bar.dart
@@ -19,6 +19,7 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
         child: const _AppBarTitle(),
       ),
       titleSpacing: titleSpacing,
+      bottom: const _AppBarBottom(),
     );
   }
 }
@@ -53,6 +54,150 @@ class _AppBarTitle extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _AppBarBottom extends StatelessWidget implements PreferredSizeWidget {
+  const _AppBarBottom();
+
+  @override
+  Size get preferredSize => Size.fromHeight(64.0.h);
+
+  @override
+  Widget build(BuildContext context) {
+    final double horizontalPadding = 16.0.w;
+    final double topPadding = 8.0.h;
+    final double bottomPadding = 24.0.h;
+
+    return Padding(
+      padding: EdgeInsets.only(
+        left: horizontalPadding,
+        top: topPadding,
+        right: horizontalPadding,
+        bottom: bottomPadding,
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          _SearchWidget(),
+          _SortButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchWidget extends StatefulWidget {
+  const _SearchWidget();
+
+  @override
+  State<_SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<_SearchWidget> {
+  final TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    searchController.addListener(
+      () => setState(() {}),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = 280.0.w;
+    final double height = 32.0.h;
+    final double verticalPadding = 8.0.h;
+    final double horizontalPadding = 12.0.w;
+    final double borderRadius = 16.0.h;
+
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.symmetric(
+        vertical: verticalPadding,
+        horizontal: horizontalPadding,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(borderRadius),
+        boxShadow: <BoxShadow>[AppShadows.innerShadow2dp],
+      ),
+      child: Row(
+        children: <Widget>[
+          const _SearchWidgetIcon(assetName: AppAssets.icSearch),
+          _SearchTextFormField(searchController: searchController),
+          Visibility(
+            visible: searchController.text.isNotEmpty,
+            child: Padding(
+              padding: EdgeInsets.only(left: verticalPadding),
+              child: InkWell(
+                child: const _SearchWidgetIcon(assetName: AppAssets.icClose),
+                onTap: () => searchController.clear(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchWidgetIcon extends StatelessWidget {
+  final String assetName;
+
+  const _SearchWidgetIcon({
+    required this.assetName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double iconSize = 12.0.h;
+
+    return SvgPicture.asset(
+      assetName,
+      colorFilter: const ColorFilter.mode(
+        AppColors.primary,
+        BlendMode.srcIn,
+      ),
+      width: iconSize,
+      height: iconSize,
+    );
+  }
+}
+
+class _SearchTextFormField extends StatelessWidget {
+  final TextEditingController searchController;
+
+  const _SearchTextFormField({required this.searchController});
+
+  @override
+  Widget build(BuildContext context) {
+    const String hintText = 'Search';
+    final double contentHorizontalPadding = 8.0.h;
+
+    return Expanded(
+      child: TextFormField(
+        controller: searchController,
+        cursorColor: AppColors.primary,
+        style: AppTextStyles.body3.copyWith(color: AppColors.dark),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: AppTextStyles.body3.copyWith(color: AppColors.medium),
+          isDense: true,
+          border: InputBorder.none,
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: contentHorizontalPadding),
+        ),
+      ),
     );
   }
 }
