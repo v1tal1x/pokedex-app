fab1cd7abaa3c7f21990335544894982ac7dedb3
v1tal1x <bvitaliy.work@gmail.com>
2024-01-27 20:53:25 +0200

    chore: Add container with scroll

--- lib/src/presentation/pages/search_page/components/body.dart
+++ lib/src/presentation/pages/search_page/components/body.dart
@@ -5,6 +5,27 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    const double bodyTopPadding = 0.0;
+    final double bodyPadding = 4.0.h;
+    final double containerBorderRadius = 8.0.h;
+
+    return Padding(
+      padding: EdgeInsets.all(bodyPadding).copyWith(top: bodyTopPadding),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(containerBorderRadius),
+        ),
+        child: DisableScrollGlowWidget(
+          child: SingleChildScrollView(
+            controller: ScrollController(),
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Container(),
+          ),
+        ),
+      ),
+    );
   }
 }
