ba2d6408aa35ad2a1f0163e990625c71efea3e06
v1tal1x <bvitaliy.work@gmail.com>
2024-01-30 20:19:32 +0200

    feat: Add pokemon item

---
+++ lib/src/presentation/pages/search_page/components/pokemon_item.dart
@@ -0,0 +1,10 @@
+part of '../search_page.dart';
+
+class _PokemonItem extends StatelessWidget {
+  const _PokemonItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
