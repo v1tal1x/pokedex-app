19575f065699fc5f2a161b25aa4added92369374
v1tal1x <bvitaliy.work@gmail.com>
2024-01-31 22:12:15 +0200

    chore: Add image for list item

--- lib/src/presentation/pages/search_page/components/pokemon_item.dart
+++ lib/src/presentation/pages/search_page/components/pokemon_item.dart
@@ -8,3 +8,28 @@ class _PokemonItem extends StatelessWidget {
     return Container();
   }
 }
+
+class _ItemImage extends StatelessWidget {
+  final String? imageUrl;
+
+  const _ItemImage({this.imageUrl});
+
+  ImageProvider _getImage() {
+    if (imageUrl == null) {
+      return const AssetImage(AppAssets.imgDefaultPokemon);
+    }
+    return NetworkImage(imageUrl!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double imageSize = 72.0.h;
+
+    return Image(
+      image: _getImage(),
+      fit: BoxFit.fitHeight,
+      width: imageSize,
+      height: imageSize,
+    );
+  }
+}
