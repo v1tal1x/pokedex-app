ebabf285fe610e2dd1195ab5ceaf563713f9f3d0
v1tal1x <bvitaliy.work@gmail.com>
2024-02-02 22:50:58 +0200

    chore: Add design to pokemon item widget

--- lib/src/presentation/pages/search_page/components/pokemon_item.dart
+++ lib/src/presentation/pages/search_page/components/pokemon_item.dart
@@ -1,18 +1,86 @@
 part of '../search_page.dart';
 
 class _PokemonItem extends StatelessWidget {
-  const _PokemonItem();
+  final PokemonEntity pokemon;
+
+  const _PokemonItem({required this.pokemon});
+
+  String capitalize(String input) {
+    if (input.isEmpty) {
+      return input;
+    }
+    return input[0].toUpperCase() + input.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double itemWidth = 104.0.w;
+    final double itemHeight = 108.0.h;
+    final double itemVerticalPadding = 4.0.h;
+    final double itemHorizontalPadding = 8.0.w;
+    final double itemBorderRadius = 8.0.h;
+    const double decorationWidth = double.infinity;
+    final double decorationHeight = 44.0.h;
+    final double decorationTopBorderRadius = 7.0.h;
+    const String idMarker = '#';
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: itemWidth,
+          height: itemHeight,
+          padding: EdgeInsets.symmetric(
+            vertical: itemVerticalPadding,
+            horizontal: itemHorizontalPadding,
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(itemBorderRadius),
+          ),
+        ),
+        Positioned(
+          bottom: 0.0,
+          child: Container(
+            width: decorationWidth,
+            height: decorationHeight,
+            decoration: BoxDecoration(
+              color: AppColors.background,
+              borderRadius: BorderRadius.circular(itemBorderRadius).copyWith(
+                topLeft: Radius.circular(decorationTopBorderRadius),
+                topRight: Radius.circular(decorationTopBorderRadius),
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  idMarker + pokemon.id.toString(),
+                  style:
+                      AppTextStyles.caption.copyWith(color: AppColors.medium),
+                ),
+              ),
+              const _ItemImage(imageUrl: null),
+              Text(
+                capitalize(pokemon.name),
+                style: AppTextStyles.body3,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
 class _ItemImage extends StatelessWidget {
   final String? imageUrl;
 
-  const _ItemImage({this.imageUrl});
+  const _ItemImage({required this.imageUrl});
 
   ImageProvider _getImage() {
     if (imageUrl == null) {
